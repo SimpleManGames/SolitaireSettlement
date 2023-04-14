ad0d205f23476e173cfbc7c3b3b2353e910d9ccb
Riley Smith <riley.smith5345@gmail.com>
2023-04-13 22:35:41 -0700

    Updated the stack action data to use the name field as asset name

--- Assets/Code/Scripts/Stack Action/StackActionData.cs
+++ Assets/Code/Scripts/Stack Action/StackActionData.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using UnityEditor;
 using UnityEngine;
 
 namespace SolitaireSettlement
@@ -7,10 +8,19 @@ namespace SolitaireSettlement
     [CreateAssetMenu(menuName = "Solitaire Settlement/Stack Action Data", fileName = "New Stack Action")]
     public class StackActionData : SerializedScriptableObject
     {
+        [field: SerializeField, Delayed]
+        private string Name { get; set; }
+
         [field: SerializeField]
         public List<CardData> NeededCardsInStack { get; private set; }
 
         [field: SerializeField]
         public IStackActionResult Result { get; private set; }
+
+        private void OnValidate()
+        {
+            var path = AssetDatabase.GetAssetPath(GetInstanceID());
+            AssetDatabase.RenameAsset(path, Name + " Stack Action");
+        }
     }
 }
